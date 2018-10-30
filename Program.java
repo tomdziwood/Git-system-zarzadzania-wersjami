314caabffc9e0698160e393fb303a82c6eef7bc4
unknown <inf132216@lab-43-6.cs.put.poznan.pl>
2018-10-30 15:55:10 +0100

    zmiana prog

--- Program.java
+++ Program.java
@@ -1,4 +1,16 @@
 public class Hello{
+	public void metoda1(){
+		System.out.println("Ta metoda nic nie zwraca");
+	}
+
+	public int trzy(){
+		return 3; // ta metoda zwraca liczbę 3
+	}
+
+	public int suma(int a, int b){
+		return a+b;  //ta metoda zwraca sumę dwóch parametrów
+	}
+	
   public static void main(String[] args){
     System.out.print("Hello World");
   }
